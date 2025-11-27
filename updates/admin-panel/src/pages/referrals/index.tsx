d4c29bf5438ea3aafb951836d27c014689aca38b
Ridwan <ibrahimridwan47@gmail.com>
2025-11-27 18:29:48 +0200

    updates

--- updates/admin-panel/src/pages/referrals/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Referral System Pages
- * Export all referral-related pages for easy importing
- */
-
-export { CampaignList } from './campaigns';
-export { TransactionList } from './transactions';
-export { RewardList } from './rewards';
-export { ReferralAnalyticsDashboard } from './analytics';
